fbf8a72d05c7a22dee9117f8aa58b8e9acf31454
danielglennross <danielglennross@gmail.com>
2017-01-12 15:06:53 +0000

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # promise-branch
-Allow branching of promise flows
+Allows isolated branching of promise flows.
+
+## Install
+`npm install --save promise-branch`
+
+## Getting started
+```javascript
+const branch = requires('promise-branch');
+
+// onResolved and onRejected are functions that return a promise
+branch(targetPromise, onResolved, onRejected);
+```
+
+`promise-branch` will ensure a rejected promise which occured before branching the flow is ignored and proxied on down the main promise chain.
+This means the error handler of `promise-branch` will only handle a rejected promise caused by the target promise.
+
+
+## Example
+```javascript
+const branch = requires('promise-branch');
+
+initialAsyncWork()
+.then(() => branch(secondaryAsyncWork(),
+  successHandler,
+  errorHandler // this only handles a rejection from secondaryAsyncWork
+))
+.then(continuedAsyncWork)
+.catch(errorAsyncWork);
+```
---
+++ test/index.spec.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const chai = require('chai');
+const expect = chai.expect;
+const assert = chai.assert;
+
+const branch = require('../lib');
+
+describe('promise branch', () => {
+
+  it('should not handle any existing rejected promises', done => {
+    Promise.reject(new Error('existing'))
+    .then(() => branch(() => Promise.resolve(),
+      () => Promise.resolve('targetResolved'),
+      () => Promise.resolve('targetRejected')
+    ))
+    .catch(err => {
+      expect(err.message).to.equal('existing');
+      done();
+    });
+  });
+
+  it('should call onResolved handler on a resolved target', done => {
+    Promise.resolve()
+    .then(() => branch(() => Promise.resolve(),
+      () => Promise.resolve('targetResolved'),
+      () => Promise.resolve('targetRejected')
+    ))
+    .then(data => {
+      expect(data).to.equal('targetResolved');
+      done();
+    });
+  });
+
+  it('should call onRejected handler on a rejected target', done => {
+    Promise.resolve()
+    .then(() => branch(() => Promise.reject('branchError'),
+      () => Promise.resolve('targetResolved'),
+      () => Promise.resolve('targetRejected')
+    ))
+    .then(data => {
+      expect(data).to.equal('targetRejected');
+      done();
+    });
+  });
+
+  it('should not handle error in down chain catch on a rejected target', done => {
+    Promise.resolve()
+    .then(() => branch(() => Promise.reject('branchError'),
+      () => Promise.resolve('targetResolved'),
+      () => Promise.resolve('targetRejected')
+    ))
+    .catch(err => {
+      assert(false);
+    })
+    .then(data => {
+      assert(true);
+      done();
+    });
+  });
+
+  it('should allow a rejected promise to be handled down chain on a rejected onResolved handler', done => {
+    Promise.resolve()
+    .then(() => branch(() => Promise.resolve(),
+      () => Promise.reject(new Error('oops')),
+      () => Promise.resolve()
+    ))
+    .catch(err => {
+      expect(err.message).to.equal('oops');
+      done();
+    });
+  });
+
+  it('should allow a rejected promise to be handled down chain on a rejected onRejected handler', done => {
+    Promise.resolve()
+    .then(() => branch(() => Promise.reject(),
+      () => Promise.resolve(),
+      () => Promise.reject(new Error('oops'))
+    ))
+    .catch(err => {
+      expect(err.message).to.equal('oops');
+      done();
+    });
+  });
+});
